47c077170ee8486cf6282a7e5c9e50b103dc77f9
Daniil Kosyanov <kosyanov.daniil@inbox.ru>
2024-11-24 23:11:05 +0500

    Task0

--- Tyuiu.KosyanovDV.Sprint5.Task0.V30.Lib/DataService.cs
+++ Tyuiu.KosyanovDV.Sprint5.Task0.V30.Lib/DataService.cs
@@ -5,8 +5,12 @@ namespace Tyuiu.KosyanovDV.Sprint5.Task0.V30.Lib
     {
         public string SaveToFileTextData(int x)
         {
-            string[] paths = { "D:", "Microsoft VS", "repos", "Tyuiu.KosyanovDV.Sprint5", "Tyuiu.KosyanovDV.Sprint5.Task0.V30", "bin", "Debug", "OutPutFileTask0.txt" };
-            string fullPath = Path.Combine(paths);
+            string path1 = "Tyuiu.KosyanovDV.Sprint5.Task0.V30";
+            string path2 = "bin";
+            string path3 = "Debug"; 
+            string path4 = "OutPutFileTask0.txt";
+            string directory = Directory.GetCurrentDirectory();
+            string fullPath = Path.Combine(directory, path1, path2, path3, path4);
             double res = Math.Round(((Math.Pow(x, 2) + 1) / (3 * x + 4)), 3);
             File.WriteAllText(fullPath, Convert.ToString(res));
             return fullPath;
